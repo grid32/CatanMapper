f70504a3a2dccee0908bea81dc9a9e70a4d7f3b7
Paul Fraser <p.fraser@live.com>
2015-07-15 18:36:43 +0800

    Finalised the generator settings GUI.

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -28,6 +28,8 @@ import java.lang.Exception;
 public class MainWindow extends JFrame
 {
 	GridBagConstraints gbc;
+	//Set up default typeCounts
+		int[] typeCounts = {0, 4, 3, 3, 4, 4, 0, 0, 0, 0}; //0:Ocean, 1:Wood, 2:Ore, 3:Clay, 4:Sheep, 5:Wheat, 6:Desert, 7:Suns, 8:Moons, 9:Gold
 
 	/** @brief Default constructor for a MainWindow.
 	 ** @details Draws all of the components for configuring the new Map.
@@ -52,14 +54,20 @@ public class MainWindow extends JFrame
 		final JPanel desertPanel = new JPanel();
 			final JSlider desertSld = new JSlider(1, 1, 1);
 			final JLabel desertLbl = new JLabel();
+			final JCheckBox goldChk = new JCheckBox();
+		final JPanel goldPanel = new JPanel();
+			final JSlider goldSld = new JSlider(0, 0, 0);
+			final JLabel goldLbl = new JLabel();
 		final JCheckBox playerChk = new JCheckBox();
 		final JCheckBox oceanChk = new JCheckBox();
 		final JCheckBox explorersChk = new JCheckBox();
 		final JPanel explorerPanel = new JPanel();
-			final JCheckBox sunChk = new JCheckBox();
-			final JCheckBox moonChk = new JCheckBox();
+			final JSlider sunSld = new JSlider(0, 15, 15);
+			final JLabel sunLbl = new JLabel();
+			final JSlider moonSld = new JSlider(0, 15, 15);
+			final JLabel moonLbl = new JLabel();
+		final JLabel typeCountLbl = new JLabel();
 		final JButton generateBtn = new JButton();
-
 
 		/////////////////////////////////////////////
 		// Size Panel							   //
@@ -117,10 +125,10 @@ public class MainWindow extends JFrame
 		// Desert Chk							   //
 		/////////////////////////////////////////////
 		desertChk.setText("Desert");
+		desertChk.setSelected(true);
 		changeGBC(0, 2, 3, 1, GridBagConstraints.NORTHWEST, 1, 0);
 		add(desertChk, gbc);
 
-		desertPanel.setVisible(false);
 		desertPanel.setLayout(new GridBagLayout());
 		changeGBC(0, 3, 3, 1, GridBagConstraints.NORTHWEST, 1, 0);
 		add(desertPanel, gbc);
@@ -139,6 +147,7 @@ public class MainWindow extends JFrame
 				{
 					setComponentVisibility(desertPanel, false);
 				}
+				updateTypeCounts(desertChk.isSelected(), desertSld.getValue(), goldChk.isSelected(), goldSld.getValue(), playerChk.isSelected(), oceanChk.isSelected(), explorersChk.isSelected(), sunSld.getValue(), moonSld.getValue(), typeCountLbl);
 			}
 		});
 
@@ -151,32 +160,84 @@ public class MainWindow extends JFrame
 		{
 			public void stateChanged(ChangeEvent e)
 			{
-				desertLbl.setText("" + desertSld.getValue());
+				desertLbl.setText(desertSld.getValue() + "/" + desertSld.getMaximum());
+				updateTypeCounts(desertChk.isSelected(), desertSld.getValue(), goldChk.isSelected(), goldSld.getValue(), playerChk.isSelected(), oceanChk.isSelected(), explorersChk.isSelected(), sunSld.getValue(), moonSld.getValue(), typeCountLbl);
 			}
 		});
 
 		changeGBC(1, 0, 1, 1, GridBagConstraints.WEST, 0, 0);
-		desertLbl.setText("" + desertSld.getValue());
+		desertLbl.setText(desertSld.getValue() + "/" + desertSld.getMaximum());
 		desertPanel.add(desertLbl, gbc);
+
+		/////////////////////////////////////////////
+		// Gold Chk								   //
+		/////////////////////////////////////////////
+		goldChk.setText("Gold");
+		changeGBC(0, 4, 3, 1, GridBagConstraints.NORTHWEST, 1, 0);
+		add(goldChk, gbc);
+
+		goldPanel.setVisible(false);
+		goldPanel.setLayout(new GridBagLayout());
+		changeGBC(0, 5, 3, 1, GridBagConstraints.NORTHWEST, 1, 0);
+		add(goldPanel, gbc);
+
+		updateGoldSld(goldSld, playerChk.isSelected(), oceanChk.isSelected());
+
+		goldChk.addItemListener(new ItemListener()
+		{
+			public void itemStateChanged(ItemEvent e)
+			{
+				if(e.getStateChange() == ItemEvent.SELECTED)
+				{
+					setComponentVisibility(goldPanel, true);
+				}
+				else
+				{
+					setComponentVisibility(goldPanel, false);
+				}
+				updateTypeCounts(desertChk.isSelected(), desertSld.getValue(), goldChk.isSelected(), goldSld.getValue(), playerChk.isSelected(), oceanChk.isSelected(), explorersChk.isSelected(), sunSld.getValue(), moonSld.getValue(), typeCountLbl);
+			}
+		});
+
+		/////////////////////////////////////////////
+		// GoldSld 								   //
+		/////////////////////////////////////////////
+		changeGBC(0, 0, 1, 1, GridBagConstraints.WEST, 1, 0);
+		goldPanel.add(goldSld, gbc);
+		goldSld.addChangeListener(new ChangeListener()
+		{
+			public void stateChanged(ChangeEvent e)
+			{
+				goldLbl.setText(goldSld.getValue() + "/" + goldSld.getMaximum());
+				updateTypeCounts(desertChk.isSelected(), desertSld.getValue(), goldChk.isSelected(), goldSld.getValue(), playerChk.isSelected(), oceanChk.isSelected(), explorersChk.isSelected(), sunSld.getValue(), moonSld.getValue(), typeCountLbl);
+			}
+		});
+
+		changeGBC(1, 0, 1, 1, GridBagConstraints.WEST, 0, 0);
+		goldLbl.setText(goldSld.getValue() + "/" + goldSld.getMaximum());
+		goldPanel.add(goldLbl, gbc);
 
 		/////////////////////////////////////////////
 		// Player Panel							   //
 		/////////////////////////////////////////////
 		playerChk.setText("6 Players");
-		changeGBC(0, 4, 3, 1, GridBagConstraints.NORTHWEST, 1, 0);
+		changeGBC(0, 6, 3, 1, GridBagConstraints.NORTHWEST, 1, 0);
 		add(playerChk, gbc);
 		playerChk.addItemListener(new ItemListener()
 		{
 			public void itemStateChanged(ItemEvent e)
 			{
 				updateDesertSld(desertSld, playerChk.isSelected(), oceanChk.isSelected());
+				updateGoldSld(goldSld, playerChk.isSelected(), oceanChk.isSelected());
+				updateSunMoonSld(sunSld, moonSld, playerChk.isSelected());
+				updateTypeCounts(desertChk.isSelected(), desertSld.getValue(), goldChk.isSelected(), goldSld.getValue(), playerChk.isSelected(), oceanChk.isSelected(), explorersChk.isSelected(), sunSld.getValue(), moonSld.getValue(), typeCountLbl);
 			}
 		});
 
 		/////////////////////////////////////////////
 		// Seafarers Chk						   //
 		/////////////////////////////////////////////
-		changeGBC(0, 5, 3, 1, GridBagConstraints.WEST, 1, 0);
+		changeGBC(0, 7, 3, 1, GridBagConstraints.WEST, 1, 0);
 		oceanChk.setText("Seafarers");
 		add(oceanChk, gbc);
 		oceanChk.addItemListener(new ItemListener()
@@ -184,19 +245,21 @@ public class MainWindow extends JFrame
 			public void itemStateChanged(ItemEvent e)
 			{
 				updateDesertSld(desertSld, playerChk.isSelected(), oceanChk.isSelected());
+				updateGoldSld(goldSld, playerChk.isSelected(), oceanChk.isSelected());
+				updateTypeCounts(desertChk.isSelected(), desertSld.getValue(), goldChk.isSelected(), goldSld.getValue(), playerChk.isSelected(), oceanChk.isSelected(), explorersChk.isSelected(), sunSld.getValue(), moonSld.getValue(), typeCountLbl);
 			}
 		});
 
 		/////////////////////////////////////////////
-		// Explorers Panel							   //
+		// Explorers Panel						   //
 		/////////////////////////////////////////////
 		explorersChk.setText("Explorers & Pirates");
-		changeGBC(0, 6, 3, 1, GridBagConstraints.NORTHWEST, 1, 0);
+		changeGBC(0, 8, 3, 1, GridBagConstraints.NORTHWEST, 1, 0);
 		add(explorersChk, gbc);
 		
 		explorerPanel.setVisible(false);
 		explorerPanel.setLayout(new GridBagLayout());
-		changeGBC(0, 7, 3, 1, GridBagConstraints.NORTHWEST, 1, 0);
+		changeGBC(0, 9, 3, 1, GridBagConstraints.NORTHWEST, 1, 0);
 		add(explorerPanel, gbc);
 
 		explorersChk.addItemListener(new ItemListener()
@@ -211,6 +274,7 @@ public class MainWindow extends JFrame
 				{
 					setComponentVisibility(explorerPanel, false);
 				}
+				updateTypeCounts(desertChk.isSelected(), desertSld.getValue(), goldChk.isSelected(), goldSld.getValue(), playerChk.isSelected(), oceanChk.isSelected(), explorersChk.isSelected(), sunSld.getValue(), moonSld.getValue(), typeCountLbl);
 			}
 		});
 
@@ -218,28 +282,62 @@ public class MainWindow extends JFrame
 		// Suns		 							   //
 		/////////////////////////////////////////////
 		changeGBC(0, 0, 1, 1, GridBagConstraints.WEST, 1, 0);
-		sunChk.setText("Suns");
-		sunChk.setSelected(true);
-		explorerPanel.add(sunChk, gbc);
+		explorerPanel.add(new JLabel("Suns"), gbc);
+
+		changeGBC(0, 1, 1, 1, GridBagConstraints.WEST, 1, 0);
+		explorerPanel.add(sunSld, gbc);
+		sunSld.addChangeListener(new ChangeListener()
+		{
+			public void stateChanged(ChangeEvent e)
+			{
+				sunLbl.setText(sunSld.getValue() + "/" + sunSld.getMaximum());
+				updateTypeCounts(desertChk.isSelected(), desertSld.getValue(), goldChk.isSelected(), goldSld.getValue(), playerChk.isSelected(), oceanChk.isSelected(), explorersChk.isSelected(), sunSld.getValue(), moonSld.getValue(), typeCountLbl);
+			}
+		});
+
+		changeGBC(1, 1, 1, 1, GridBagConstraints.WEST, 0, 0);
+		sunLbl.setText(sunSld.getValue() + "/" + sunSld.getMaximum());
+		explorerPanel.add(sunLbl, gbc);
 
 		/////////////////////////////////////////////
 		// Moons	 							   //
 		/////////////////////////////////////////////
-		changeGBC(0, 1, 1, 1, GridBagConstraints.WEST, 1, 0);
-		moonChk.setText("Moons");
-		moonChk.setSelected(true);
-		explorerPanel.add(moonChk, gbc);
+		changeGBC(0, 2, 1, 1, GridBagConstraints.WEST, 1, 0);
+		explorerPanel.add(new JLabel("Moons"), gbc);
+
+		changeGBC(0, 3, 1, 1, GridBagConstraints.WEST, 1, 0);
+		explorerPanel.add(moonSld, gbc);
+		moonSld.addChangeListener(new ChangeListener()
+		{
+			public void stateChanged(ChangeEvent e)
+			{
+				moonLbl.setText(moonSld.getValue() + "/" + moonSld.getMaximum());
+				updateTypeCounts(desertChk.isSelected(), desertSld.getValue(), goldChk.isSelected(), goldSld.getValue(), playerChk.isSelected(), oceanChk.isSelected(), explorersChk.isSelected(), sunSld.getValue(), moonSld.getValue(), typeCountLbl);
+			}
+		});
+
+		changeGBC(1, 3, 1, 1, GridBagConstraints.WEST, 0, 0);
+		moonLbl.setText(moonSld.getValue() + "/" + moonSld.getMaximum());
+		explorerPanel.add(moonLbl, gbc);
+
+		/////////////////////////////////////////////
+		// typeCounts 							   //
+		/////////////////////////////////////////////
+		changeGBC(1, 10, 3, 1, GridBagConstraints.NORTHWEST, 1, 0);
+		updateTypeCounts(desertChk.isSelected(), desertSld.getValue(), goldChk.isSelected(), goldSld.getValue(), playerChk.isSelected(), oceanChk.isSelected(), explorersChk.isSelected(), sunSld.getValue(), moonSld.getValue(), typeCountLbl);
+		add(typeCountLbl, gbc);
+
 
 		/////////////////////////////////////////////
 		// Spacing	 							   //
 		/////////////////////////////////////////////
-		changeGBC(0, 8, 3, 1, GridBagConstraints.NORTHWEST, 1, 1);
+		changeGBC(0, 11, 3, 1, GridBagConstraints.NORTHWEST, 1, 1);
 		add(new JPanel(), gbc);
 
 		/////////////////////////////////////////////
 		// GenerateBtn 							   //
 		/////////////////////////////////////////////
-		changeGBC(1, 9, 1, 1, GridBagConstraints.NORTHWEST, 1, 0);
+		changeGBC(1, 12, 1, 1, GridBagConstraints.NORTHWEST, 1, 0);
 		generateBtn.setText("Generate");
 		generateBtn.addActionListener(new ActionListener()
 		{
@@ -387,6 +485,140 @@ public class MainWindow extends JFrame
 		this.setVisible(true);
 	}
 
+	/** @brief Updates the typeCountLbl.
+	 ** @param inLabel The JLabel for changing.
+	 ** @return void Returns nothing.
+	 **/
+	void changeTypeCountLbl(JLabel inLabel)
+	{
+		String typeCountString = "<html>";
+		int i;
+		for(i = 0; i < typeCounts.length; i++)
+		{
+			if(typeCounts[i] != 0)
+			{
+				String type;
+				switch(i)
+				{
+					case 0:
+							type = "Ocean";
+							break;
+					case 1:
+							type = "Wood";
+							break;
+					case 2:
+							type = "Ore";
+							break;
+					case 3:
+							type = "Clay";
+							break;
+					case 4:
+							type = "Sheep";
+							break;
+					case 5:
+							type = "Wheat";
+							break;
+					case 6:
+							type = "Desert";
+							break;
+					case 7:
+							type = "Sun";
+							break;
+					case 8:
+							type = "Moon";
+							break;
+					case 9:
+							type = "Gold";
+							break;
+					default:
+							type = "Undefined";
+							break;
+				}
+				typeCountString += typeCounts[i] + " " + type;
+				if(typeCounts[i] > 1)
+					typeCountString += " tiles available.<br>";
+				else
+					typeCountString += " tile available.<br>";
+
+			}
+		}
+		typeCountString += "</html";
+		inLabel.setText(typeCountString);
+	}
+
+	/** @brief Updates the maximum type count of each type.
+	 ** @param inDesertFlag The return of desertChk.isSelected().
+	 ** @param inDesertVal The value of the desertSld.
+	 ** @param inPlayerFlag The return of playerChk.isSelected().
+	 ** @param inOceanFlag The return of oceanChk.isSelected().
+	 ** @param inExplorersFlag The return of explorersChk.isSelected().
+	 ** @param inSunVal The value of the sunSld.
+	 ** @param inMoonVal The value of the moonSld.
+	 ** @param inLabel The label to contain the list of type counts.
+	 ** @return void Returns nothing.
+	 **/
+	void updateTypeCounts(boolean inDesertFlag, int inDesertVal, boolean inGoldFlag, int inGoldVal, boolean inPlayerFlag, boolean inOceanFlag, boolean inExplorersFlag, int inSunVal, int inMoonVal, JLabel inLabel)
+	{
+		int[] tempCounts = {0, 4, 3, 3, 4, 4, 0, 0, 0, 0}; //0:Ocean, 1:Wood, 2:Ore, 3:Clay, 4:Sheep, 5:Wheat, 6:Desert, 7:Suns, 8:Moons, 9:Gold
+		typeCounts = tempCounts;
+		if(inPlayerFlag)
+		{
+			typeCounts[1] += 2;
+			typeCounts[2] += 2;
+			typeCounts[3] += 2;
+			typeCounts[4] += 2;
+			typeCounts[5] += 2;
+		}
+		if(inDesertFlag)
+		{
+			typeCounts[6] = inDesertVal;
+		}
+		if(inGoldFlag)
+		{
+			typeCounts[9] = inGoldVal;
+		}
+		if(inOceanFlag)
+		{
+			typeCounts[0] += 19;
+			typeCounts[1] += 1;
+			typeCounts[2] += 2;
+			typeCounts[3] += 2;
+			typeCounts[4] += 1;
+			typeCounts[5] += 1;
+			if(inPlayerFlag)
+			{
+				typeCounts[0] += 7;
+			}
+		}
+		if(inExplorersFlag)
+		{
+			typeCounts[7] += inSunVal;
+			typeCounts[8] += inMoonVal;
+		}
+		changeTypeCountLbl(inLabel);
+		this.pack();
+	}
+
+	/** @brief Updates the maximum allowance of sun/moon tiles.
+	 ** @param inSun The JSlider for sun tiles.
+	 ** @param inMoon The JSlider for moon tiles.
+	 ** @param inPlayerFlag The return of playerChk.isSelected().
+	 ** @return void Returns nothing.
+	 **/
+	void updateSunMoonSld(JSlider inSun, JSlider inMoon, boolean inPlayerFlag)
+	{
+		if(inPlayerFlag)
+		{
+			inSun.setMaximum(20);
+			inMoon.setMaximum(20);
+		}
+		else
+		{
+			inSun.setMaximum(15);
+			inMoon.setMaximum(15);
+		}
+	}
+
 	/** @brief Updates the maximum for desertSld.
 	 ** @param inSlider The JSlider desertSld.
 	 ** @param inPlayerFlag The return of playerChk.isSelected().
@@ -411,6 +643,31 @@ public class MainWindow extends JFrame
 		}
 
 		inSlider.setMaximum(desMax);
+	}
+
+	/** @brief Updates the maximum for goldSld.
+	 ** @param inSlider The JSlider goldSld.
+	 ** @param inPlayerFlag The return of playerChk.isSelected().
+	 ** @param inOceanFlag The return of oceanChk.isSelected().
+	 ** @return void Returns nothing.
+	 **/
+	void updateGoldSld(JSlider inSlider, boolean inPlayerFlag, boolean inOceanFlag)
+	{
+		//Count max deserts.
+		int goldMax = 0;
+		if(inOceanFlag)
+		{
+			goldMax += 2;
+			if(inPlayerFlag)
+			{
+				goldMax += 2;
+			}
+		}
+		if(goldMax == 0)
+			inSlider.setMinimum(0);
+		else
+			inSlider.setMinimum(1);
+		inSlider.setMaximum(goldMax);
 	}
 
 	/** @brief Functions to change values of the GridBagConstraints object.
